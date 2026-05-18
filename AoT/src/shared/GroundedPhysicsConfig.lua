4be0b0ad686b05aa5a9866e3fe964a143cb7c3a0
Priyansu Biswal <pribiswal7@gmail.com>
2026-05-17 22:21:19 -0500

    Mirror live Roblox code layout in Rojo

---
+++ AoT/src/shared/GroundedPhysicsConfig.lua
@@ -0,0 +1,12 @@
+local GroundedPhysicsConfig = {
+	Gravity = 196.2,
+	FreefallAcceleration = 110,
+	TerminalFallSpeed = 90,
+	AirControlResponsiveness = 6,
+	AirControlMaxSpeed = 24,
+	AirDashSpeed = 72,
+	AirDashDebounce = 0.15,
+	DoubleTapWindow = 0.25,
+}
+
+return GroundedPhysicsConfig
